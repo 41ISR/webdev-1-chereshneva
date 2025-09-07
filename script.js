1e1761a034364e24cc29084e0c8ce859f59a2874
Else <leveychel@gmail.com>
2025-09-07 15:14:50 +0000

    Completed (calculator): percentage function added

--- script.js
+++ script.js
@@ -8,16 +8,23 @@ function handleButton(button) {
 
     // Обработка разных типов кнопок
     if (value === '=') {
-      resultField.textContent = eval(resultField.textContent.replaceAll("×", "*").replaceAll("−", "-").replaceAll("÷", "/").replaceAll("+", "+"));
+      if (resultField.textContent.includes('%')){
+            let n1 = resultField.textContent.split('%');
+            resultField.textContent = (n1[0] / 100) * n1[1];
+            resultField.textContent += '%';
+        }
+        else {
+            resultField.textContent = eval(resultField.textContent.replaceAll("×", "*").replaceAll("−", "-").replaceAll("÷", "/").replaceAll("+", "+"));
+        }
     } 
     else if (value === 'AC') {
       resultField.textContent = '';
     } 
     else if(value === '%'){
-      resultField.textContent = eval(resultField.textContent) + '%';
+      resultField.textContent = eval(resultField.textContent.replaceAll("×", "*").replaceAll("−", "-").replaceAll("÷", "/").replaceAll("+", "+")) + '%';
     }
     else if(value === '+/-'){
-      resultField.textContent = '-' + eval(resultField.textContent);
+       resultField.textContent = eval(resultField.textContent.replaceAll("×", "*").replaceAll("−", "-").replaceAll("÷", "/").replaceAll("+", "+"))* -1;
     }
     else {
       resultField.textContent += value;
